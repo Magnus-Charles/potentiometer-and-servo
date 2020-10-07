66f530f8975bd8fee7c240253a1ed6e9fb53aac0
Magnus-Charles <71400268+Magnus-Charles@users.noreply.github.com>
2020-10-07 19:34:49 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,4 @@
+let potentiometer = 0
 basic.showLeds(`
     # . . . #
     . # . # .
@@ -6,9 +7,8 @@ basic.showLeds(`
     # . . . #
     `)
 basic.forever(function () {
-    robotbit.Servo(robotbit.Servos.S1, 0)
-    basic.pause(100)
-    robotbit.Servo(robotbit.Servos.S1, 180)
-    basic.pause(100)
+    potentiometer = pins.analogReadPin(AnalogPin.P1)
+    robotbit.Servo(robotbit.Servos.S1, potentiometer)
     basic.showNumber(pins.analogReadPin(AnalogPin.P1))
+    basic.pause(100)
 })
