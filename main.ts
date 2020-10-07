c48499c5ce829ee9d07ba8d5736dfbeec74037b9
Magnus-Charles <71400268+Magnus-Charles@users.noreply.github.com>
2020-10-07 18:53:10 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,13 @@
+basic.showLeds(`
+    # . . . #
+    . # . # .
+    . # # # .
+    . # . # .
+    # . . . #
+    `)
+basic.forever(function () {
+    robotbit.Servo(robotbit.Servos.S1, 0)
+    basic.pause(1000)
+    robotbit.Servo(robotbit.Servos.S1, 180)
+    basic.pause(1000)
+})
