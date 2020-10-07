7d437116ac8f7c468579b7822c77fceaf24674a8
Magnus-Charles <71400268+Magnus-Charles@users.noreply.github.com>
2020-10-07 18:56:39 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -7,8 +7,8 @@ basic.showLeds(`
     `)
 basic.forever(function () {
     robotbit.Servo(robotbit.Servos.S1, 0)
-    basic.pause(1000)
+    basic.pause(100)
     robotbit.Servo(robotbit.Servos.S1, 180)
-    basic.pause(1000)
+    basic.pause(100)
     basic.showNumber(pins.analogReadPin(AnalogPin.P1))
 })
